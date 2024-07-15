2120459bd17a884efe3c5382894c7b51f8201b3e
4L3X <alexandreaugepro@gmail.com>
2024-07-15 09:39:36 +0200

    Mise à jour de la page d'accueil

--- netflim-app/src/component/MovieList.jsx
+++ netflim-app/src/component/MovieList.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import { MovieCard } from "./MovieCard";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import './MovieList.css';
 
-export const MovieList  =() => {
-  return(
-    <div>
-      <p>Films populaire</p>
-      <MovieCard/>
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const apiKey = '0ea6622414dacb5eff8f08858f745c6e'; // Remplacez par votre clé API
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr-FR&page=1`)
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des films populaires:', error);
+      });
+  }, []);
+
+  return (
+    <div className='content'>
+      <h2>Les plus populaires</h2>
+      <div className='film-container'>
+        {movies.map(movie => (
+          <div key={movie.id} className='poster-container'>
+            <Link to ="/MovieDetaillsPage">
+              <img src={`${baseImageUrl}${movie.poster_path}`} alt={movie.title}/>
+            </Link>
+
+          </div>
+        ))};
+      </div>
+
     </div>
   );
 };
