ea75033d051fb49628db62876b1de71f63602b8b
4L3X <alexandreaugepro@gmail.com>
2024-07-22 16:56:53 +0200

    Update des favoris affichés

--- netflim-app/src/component/MovieDetailsPage.jsx
+++ netflim-app/src/component/MovieDetailsPage.jsx
@@ -24,8 +24,8 @@ const MovieDetailsPage = () => {
       console.error('Erreur lors de la récupération des détails du film:', error);
     });
 
-  // Fetch movie credits (cast)
-  fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=fr-FR`)
+    // Fetch movie credits (cast)
+    fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=fr-FR`)
     .then(response => response.json())
     .then(data => {
       setCast(data.cast);
@@ -38,103 +38,106 @@ const MovieDetailsPage = () => {
     });
 
     fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=fr-FR`)
-          .then(response => response.json())
-          .then(trailersData => {
-            const trailersList = trailersData.results.filter(video => video.type === 'Trailer' && video.site === 'YouTube');
-            setTrailers(trailersList);
-            console.log('Video data :', trailersData);
-          })
-          .catch(error => {
-            console.error('Erreur lors de la récupération des trailers du film:', error);
-          });
-}, [id, apiKey]);
-
-const checkIfFavorite = (movieId) => {
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  const isFav = favorites.some(favMovie => favMovie.id === movieId);
-  setIsFavorite(isFav);
-};
-
-const handleFavoriteClick = () => {
-  const favStarIcon = document.getElementById('fav-star');
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  if (isFavorite) {
-    const newFavorites = favorites.filter(favMovie => favMovie.id !== movie.id);
-    localStorage.setItem('favorites', JSON.stringify(newFavorites));
-    setIsFavorite(false);
-    favStarIcon.style.color = ''; // ou utilisez la couleur initiale que vous voulez
-  } else {
-    favorites.push({
-      id: movie.id,
-      title: movie.title,
-      poster_path: movie.poster_path,
-      release_date: movie.release_date
+    .then(response => response.json())
+    .then(trailersData => {
+      const trailersList = trailersData.results.filter(video => video.type === 'Trailer' && video.site === 'YouTube');
+      setTrailers(trailersList);
+      console.log('Video data :', trailersData);
+    })
+    .catch(error => {
+      console.error('Erreur lors de la récupération des trailers du film:', error);
     });
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-    setIsFavorite(true);
-    favStarIcon.style.color = 'yellow';
+  }, [id, apiKey]);
+
+  const checkIfFavorite = (movieId) => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const isFav = favorites.some(favMovie => favMovie.id === movieId);
+    setIsFavorite(isFav);
+  };
+
+  const handleFavoriteClick = () => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const dialog = document.querySelector("dialog");
+
+    if (isFavorite) {
+      const newFavorites = favorites.filter(favMovie => favMovie.id !== movie.id);
+      localStorage.setItem('favorites', JSON.stringify(newFavorites));
+      setIsFavorite(false);
+    } else {
+      dialog.showModal();
+      requestAnimationFrame(() => {
+        dialog.classList.add('show');
+      });
+
+      // Fermer automatiquement le dialog après 1.5 secondes (1500 millisecondes)
+      setTimeout(() => {
+        dialog.classList.remove('show'); // Retire la classe pour l'animation de fermeture
+        setTimeout(() => dialog.close(), 500); // Attend la fin de l'animation avant de fermer
+      }, 1500);
+
+      favorites.push({
+        id: movie.id,
+        title: movie.title,
+        poster_path: movie.poster_path,
+        release_date: movie.release_date
+      });
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+      setIsFavorite(true);
+    }
+  };
+
+  if (!movie) {
+    return <div>Loading...</div>;
   }
-};
-
-if (!movie) {
-  return <div>Loading...</div>;
-}
-
-
-
 
   return (
-    <div className='movie-wrapper' >
+    <div className='movie-wrapper'>
       <div className="left-container">
         <img src={`${baseImageUrl}${movie.poster_path}`} alt={movie.title} />
-        <button id="fav-star-button" onClick={handleFavoriteClick}><i id ="fav-star" class="fa-solid fa-star custom-star"></i></button>
-        
-        <button><i class="fa-solid fa-play custom-star"></i></button>
-        <i class="fa-solid fa-star custom-star"></i>
+        <button id="fav-star-button" onClick={handleFavoriteClick}>
+          <i id="fav-star" className="fa-solid fa-star custom-star" style={{ color: isFavorite ? 'yellow' : '' }}></i>
+        </button>
+        <dialog id="add-fav-dialog">
+          <p><strong>{movie.title}<br/></strong> a été ajouté à vos favoris !</p>
+        </dialog>
       </div>
       <div className="right-container">
         <div className="movie-info-container">
-          <h1>{movie.title} </h1> 
+          <h1>{movie.title}</h1>
           <p className='movie-director'>{director}</p>
           <p> {movie.release_date}, {movie.genres.map(genre => genre.name).join(' / ')}</p>
           <p>Note : {movie.vote_average}</p>
-          
-          {cast.length > 0 ? (
-          <ul className='movie-actor-list'>
-            {cast.slice(0, 5).map(actor => (  // Affiche uniquement les 5 premiers acteurs
-              <li key={actor.cast_id} className='movie-actor'>
-                {actor.profile_path ? (
-                  <img
-                    src={`${baseImageUrl}${actor.profile_path}`}
-                    alt={actor.name}
 
-                  />
-                ) : (
-                  <div class="no-movie-actor"/>
-                )}
-                {actor.name}, {actor.character}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>Aucun cast disponible.</p>
-        )}
-          
+          {cast.length > 0 ? (
+            <ul className='movie-actor-list'>
+              {cast.slice(0, 5).map(actor => (
+                <li key={actor.cast_id} className='movie-actor'>
+                  {actor.profile_path ? (
+                    <img
+                      src={`${baseImageUrl}${actor.profile_path}`}
+                      alt={actor.name}
+                    />
+                  ) : (
+                    <div className="no-movie-actor" />
+                  )}
+                  {actor.name}, {actor.character}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Aucun cast disponible.</p>
+          )}
         </div>
         <div className="movie-synopsis">
           <p>Synopsis : </p>
           <p className="paragraph-syno">{movie.overview}</p>
         </div>
-        
       </div>
       <div className="background-img-movie">
-      <img src={`${baseImageUrl}${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${baseImageUrl}${movie.backdrop_path}`} alt={movie.title} />
       </div>
     </div>
   );
 };
-
-
-
 
 export default MovieDetailsPage;
--- netflim-app/src/component/ProfilePage.jsx
+++ netflim-app/src/component/ProfilePage.jsx
@@ -6,52 +6,53 @@ import './ProfilePage.css';
 import './SideBar.css';
 
 const ProfilePage = () => {
-    const [favorites, setFavorites] = useState([]);
-  
-    useEffect(() => {
-      const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      setFavorites(storedFavorites);
-    }, []);
-  
+  const [favorites, setFavorites] = useState([]);
+  const [numMoviesToShow, setNumMoviesToShow] = useState(5);
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  const showMoreMovies = () => {
+    setNumMoviesToShow(numMoviesToShow + 5); // Augmente le nombre de films affichés par 5
+  };
+
   return (
     <div className='profil-page-wrapper'>
-      <Sidebar className="side"/>
+      <Sidebar className="side" />
       <span className='sidebar-border'></span>
       <div className="Profilcontainer">
         <div className='profil-page-info'>
-          <img src={imageprofile} alt="Profil"/>
+          <img src={imageprofile} alt="Profil" />
           <div className='profil-page-pseudo-mail'>
             <p>Pseudo</p>
             <p>email@gmail.com</p>
           </div>
         </div>
 
-      <div class="fav-container">
-        <div>
-          <h2>Mes films favoris</h2>
-        </div>
-        <div className='fav-movie-list'>
-        {favorites.length === 0 ? (
-          <p>Aucun film en favoris.</p>
-        ) : (
-          favorites.slice(0,5).map((movie) => (
-            <div key={movie.id} className="favorite-movie">
-              <Link to={`/MovieDetailsPage/${movie.id}`}>
-                <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
-              </Link>
-            </div>
-          ))
-        )}
+        <div className="fav-container">
+          <div className="fav-container-title">
+            <h2>Mes films favoris</h2>
+            <button onClick={showMoreMovies}>+</button>
+          </div>
+          <div className='fav-movie-list'>
+            {favorites.length === 0 ? (
+              <p>Aucun film en favoris.</p>
+            ) : (
+              favorites.slice(0, numMoviesToShow).map((movie) => (
+                <div key={movie.id} className="favorite-movie">
+                  <Link to={`/MovieDetailsPage/${movie.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
+                  </Link>
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </div>
     </div>
-    </div>
   );
-  
-
 };
-
-
-
 
 export default ProfilePage;
